bce260b333e35a9f0e1035fb68023bdd844846ae
dasWarder <Al.94_94@mail.ru>
2021-09-27 16:50:04 +0300

    Add Controller tests for UserContractController

--- crm-common/service-maper/target/generated-sources/annotations/com/example/mapper/ProfileMapperImpl.java
+++ crm-common/service-maper/target/generated-sources/annotations/com/example/mapper/ProfileMapperImpl.java
@@ -8,8 +8,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-09-24T08:48:05+0300",
-    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.10 (AdoptOpenJDK)"
+    date = "2021-09-24T16:05:02+0300",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 16.0.1 (Oracle Corporation)"
 )
 @Component
 public class ProfileMapperImpl implements ProfileMapper {
